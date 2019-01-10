de1edb26b0fa9b2af0023f3b69c1b0905722ef2d
Colleen E Higgins <colleenhiggins87@gmail.com>
2019-01-10 05:03:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var whatTheyPlay = {};
+  for (i = 0, i < musicians, i++ || i=0, i < instruments, i++){
+    whatTheyPlay = musicians[0] " play the " instruments[0]
+  }
+  return whatTheyPlay;
+}
