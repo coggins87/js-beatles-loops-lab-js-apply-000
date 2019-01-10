71f06fc79abfa9998931b3c2f33ab1af01b3cad2
Colleen E Higgins <colleenhiggins87@gmail.com>
2019-01-10 05:19:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments){
   var whatTheyPlay = [];
-  for (i = 0; i <= musicians.length; i++) {
+  for (musicians[] = 0; musicians[] <= musicians.length; musicians[]++) {
     whatTheyPlay = musicians[0] + " plays the " + instruments[0]
   }
   return whatTheyPlay;
